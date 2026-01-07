dc03614bfe4034da84b146f4b1fed7c9db32d6ed
vikram-42-alla <allatrivikram@gmail.com>
2026-01-07 19:56:29 +0530

    refactor: replace Google Generative AI with Axios for expense categorization

--- ai-service/Controller/aiController.js
+++ ai-service/Controller/aiController.js
@@ -1,27 +1,23 @@
-const {GoogleGenerativeAI}=require("@google/generative-ai")
+const axios=require("axios")
 
 const express=require("express")
 const app=express()
 const dotenv=require("dotenv")
 dotenv.config()
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
- async function categorizeItem(itemName) {
-  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
-
+async function categorizeItem(itemName) {
   const prompt = `
   Categorize the following expense item into one of these categories:
   [Food, Dairy, Transport, Entertainment, Health, Shopping, Bills, Education, Others].
   Item: "${itemName}"
   Return only the category name.
   `;
-
-  const result = await model.generateContent(prompt);
-  const category = result.response.text().trim();
-
+  const response = await axios.post(`process.env.AI_URL/chat`, { prompt });
+  const category = response.data.response;
+ 
   return category;
 }
+
 app.post('/category',async(req,res)=>{
   try {
     const {item}=req.body;
